0fff38c670abd23b9dc89ccc33a093915d638f33
Crez <crez@Christophers-MacBook-Pro.local>
2019-03-29 17:01:59 +0000

    big update

--- src/main/java/trainer/Trainer.java
+++ src/main/java/trainer/Trainer.java
@@ -9,6 +9,7 @@ import tts.Tts;
 
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.util.Arrays;
 
 public class Trainer implements KeyListener {
 
@@ -16,20 +17,40 @@ public class Trainer implements KeyListener {
   public Tts tts;
 
   boolean started = false;
-  BuildOrder pvx;
-  BuildOrder pvz;
-  BuildOrder pvp;
-  BuildOrder pvt;
+  // x = random, z = zerg, p = protoss, t = terran
+  BuildOrder x;
+  BuildOrder z;
+  BuildOrder p;
+  BuildOrder t;
+  BuildOrder q;
 
   public void init() {
     try {
-      pvx = BuildOrderFileReader.readBuildOrderFile(new File("/Users/crez/Desktop/sc2trainer/src/main/resources/PvX/easyBuild"));
-      pvz = BuildOrderFileReader.readBuildOrderFile(new File("/Users/crez/Desktop/sc2trainer/src/main/resources/PvZ/oracleZealotAllIn"));
-      pvp = BuildOrderFileReader.readBuildOrderFile(new File("/Users/crez/Desktop/sc2trainer/src/main/resources/PvP/blinkAllIn"));
-      pvt = BuildOrderFileReader.readBuildOrderFile(new File("/Users/crez/Desktop/sc2trainer/src/main/resources/PvT/collossusPheonixVoidRay"));
+      x = BuildOrderFileReader.readBuildOrderFile(new File("/Users/crez/Desktop/sc2trainer/src/main/resources/PvX/easyBuild"));
+      z = BuildOrderFileReader.readBuildOrderFile(new File("/Users/crez/Desktop/sc2trainer/src/main/resources/PvZ/oracleZealotAllIn"));
+      p = BuildOrderFileReader.readBuildOrderFile(new File("/Users/crez/Desktop/sc2trainer/src/main/resources/PvP/AdeptAllIn"));
+//      p = BuildOrderFileReader.readBuildOrderFile(new File("/Users/crez/Desktop/sc2trainer/src/main/resources/TVP"));
+//      t = BuildOrderFileReader.readBuildOrderFile(new File("/Users/crez/Desktop/sc2trainer/src/main/resources/TvT"));
+//      z = BuildOrderFileReader.readBuildOrderFile(new File("/Users/crez/Desktop/sc2trainer/src/main/resources/TvZ"));
+
+
+
+      //t = BuildOrderFileReader.readBuildOrderFile(new File("/Users/crez/Desktop/sc2trainer/src/main/resources/PvT/collossusPheonixVoidRay"));
+      t = BuildOrderFileReader.readBuildOrderFile(new File("/Users/crez/Desktop/sc2trainer/src/main/resources/PvT/collossusPheonixVoidRay"));
+      q = BuildOrderFileReader.readBuildOrderFile(new File("/Users/crez/Desktop/sc2trainer/src/main/resources/PvX/zealotImmoArchon"));
+      z = BuildOrderFileReader.readBuildOrderFile(new File("/Users/crez/Desktop/sc2trainer/src/main/resources/PvX/zealotImmoArchon"));
+//      z = BuildOrderFileReader.readBuildOrderFile(new File("/Users/crez/Desktop/sc2trainer/src/main/resources/ZvZ/droneAllIn"));
+//      p = BuildOrderFileReader.readBuildOrderFile(new File("/Users/crez/Desktop/sc2trainer/src/main/resources/ZvP/safeBroodRush"));
+//      t = BuildOrderFileReader.readBuildOrderFile(new File("/Users/crez/Desktop/sc2trainer/src/main/resources/ZvT/economic"));
+
+//      p = BuildOrderFileReader.readBuildOrderFile(new File("/Users/crez/Desktop/sc2trainer/src/main/resources/TVP"));
+//      t = BuildOrderFileReader.readBuildOrderFile(new File("/Users/crez/Desktop/sc2trainer/src/main/resources/TvT"));
+//      z = BuildOrderFileReader.readBuildOrderFile(new File("/Users/crez/Desktop/sc2trainer/src/main/resources/TvZ"));
     } catch (FileNotFoundException e) {
       e.printStackTrace();
     }
+
+
 
   }
 
@@ -40,6 +61,7 @@ public class Trainer implements KeyListener {
     startTime = System.currentTimeMillis();
     tts.textToSpeech(buildOrder.getBuildOrderName());
     buildOrder.getEventList().forEach(this::processEvent);
+    tts.textToSpeech("Build order complete. Free build.");
     tts.shutdown();
   }
 
@@ -65,22 +87,27 @@ public class Trainer implements KeyListener {
     System.out.println(key);
     if(!started){
       if( key.equals("X")){
-        trainBuildOrder(pvx);
+        trainBuildOrder(x);
         started = true;
       }
 
       if( key.equals("T")){
-        trainBuildOrder(pvt);
+        trainBuildOrder(t);
         started = true;
       }
 
       if( key.equals("P")){
-        trainBuildOrder(pvp);
+        trainBuildOrder(p);
         started = true;
       }
 
       if( key.equals("Z")){
-        trainBuildOrder(pvz);
+        trainBuildOrder(z);
+        started = true;
+      }
+
+      if( key.equals("Q")){
+        trainBuildOrder(q);
         started = true;
       }
     }
